fcfd5fb28770b816835445a043899ed78a93a381
kamzie <mrkamz@gmail.com>
2024-06-04 11:45:43 +0100

    Added details description to ping_server script

--- 05_ping_servers.sh
+++ 05_ping_servers.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# This script pings a list of servers and reports their status.
+# This script pings a list of servers and reports their health status.
 
-SERVER_LIFE='/vagrant/servers'
+# Server list file location (modify as needed).
+SERVER_LIFE='/vagrant/05_servers'
 
+# Check if server list file exists.
 if [[ ! -e ${SERVER_LIFE} ]]; then
   echo "Cannot open ${SERVER_LIFE}." >&2
   exit 1
 fi
 
+# Iterate through server list to check status.
 for SERVER in $(cat ${SERVER_LIFE}); do
        	echo "Pinging ${SERVER}"
   ping -c 1 ${SERVER} &> /dev/null
   
   if [[ $? -ne 0 ]]; then
     echo "${SERVER} down."
+    echo  
   else
     echo "${SERVER} is up."
-   fi
+    echo
+  fi
 done
+
+exit 0
+
 
